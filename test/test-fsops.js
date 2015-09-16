8ff00141843e362b7f610e3219cdeb12ea6b05a1
AshwinH <ashwinh@cybage.com>
2015-09-16 15:58:30 +0530

    Fixes #12 : initial relevant test cases

--- test/test-fsops.js
+++ test/test-fsops.js
@@ -1,10 +1,45 @@
 (function() {
 	'use strict';
 
-	exports.dummytest = function(test) {
-		test.expect(1);
-		test.ok(true, "this assertion should pass");
-		test.done();
+	/**
+	 * Requires a frontcore-cli utility functions;
+	 * @requires fsops:../lib/fsops.js
+	 */
+	var fsops = require('../lib/fsops.js');
+
+	/**
+	 * Expected JSON objects to hold data that are provided to source to generated datafiles;
+	 */
+	var config_projectname = require('./expected/projectname.json'),
+		config_language = require('./expected/language.json'),
+		config_directories = require('./expected/directories.json');
+
+	/**
+	 * Includes setup, teardown and unit test assertions
+	 * @module ./test/test-fsops.js
+	 * @access public
+	 */
+	module.exports = {
+
+		setUp: function(callback) {
+			fsops.writeJSONFile(config_projectname, __dirname + '/tmp/config_projectname.json');
+			fsops.writeJSONFile(config_language, __dirname + '/tmp/config_language.json');
+			fsops.writeJSONFile(config_directories, __dirname + '/tmp/config_directories.json');
+			callback();
+		},
+
+		tearDown: function(callback) {
+			callback();
+		},
+
+		GENERATE_CONFIG_FILE_TEST: function(test) {
+
+			test.deepEqual(require('./tmp/config_projectname.json'), config_projectname);
+			test.deepEqual(require('./tmp/config_language.json'), config_language);
+			test.deepEqual(require('./tmp/config_directories.json'), config_directories);
+
+			test.done();
+		}
 	};
 
 })();
