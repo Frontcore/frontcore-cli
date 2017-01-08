16467d6ef50e302afee3242799e1ca0b737e8da1
hegdeashwin <ashwin.hegde3@gmail.com>
2017-01-09 02:04:09 +0530

    remove grunt-jsonlint/travis-lint plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,21 +14,6 @@
 		 * Initialize grunt initial configuration
 		 */
 		grunt.initConfig({
-
-			/**
-			 * Run jsonlint on JavaScript files
-			 */
-			'jsonlint': {
-				files: '*.json'
-			},
-
-			/**
-			 * Run lint on .travis.yml
-			 */
-			'travis-lint': {
-				files: '.travis.yml'
-			},
-
 			/**
 			 * Run unit tests
 			 */
@@ -40,23 +25,11 @@
 		/**
 		 * Load grunt plugins (npm)
 		 */
-		grunt.loadNpmTasks('grunt-jsonlint');
 		grunt.loadNpmTasks('grunt-contrib-nodeunit');
-		grunt.loadNpmTasks('grunt-travis-lint');
 
 		/**
 		 * Set `default` task
 		 */
-		grunt.registerTask('default', ['jsonlint', 'nodeunit']);
-
-		/**
-		 * Set `lintbuild` task
-		 */
-		grunt.registerTask('lintbuild', ['travis-lint']);
-
-		/**
-		 * Set `test` task
-		 */
-		grunt.registerTask('test', ['nodeunit']);
+		grunt.registerTask('default', ['nodeunit']);
 	};
 })();
